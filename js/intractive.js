9f617fd59b5263eee64e492824692fe1decd2fc9
Ahmed <eng.ahmedkhaled53@gmail.com>
2018-12-12 23:39:45 +0200

    main page add form student

--- js/intractive.js
+++ js/intractive.js
@@ -1,13 +1,81 @@
 $(document).ready(function(){
 	//code
-	$('.dropdown').click(function(){
+	/*$('.dropdown').click(function(){
 		$(this).toggleClass('show');
 		$(this).children('.dropdown-menu').toggleClass('show');
 
 	});
+	$('body').click(function(e) {
+	    if ($(e.target).closest('.dropdown').length === 0) {
+	    	$('.dropdown').removeClass('show');
+			$('.dropdown').children('.dropdown-menu').removeClass('show');
+	    }
+	});*/
+	$(window).click(function() {
+	//Hide the menus if visible
+	if($('.dropdown-menu').css('display') == 'block')
+		$('.dropdown').removeClass('show');
+		$('.dropdown').children('.dropdown-menu').removeClass('show');
+	});
 
+	$('.dropdown').click(function(event){
+	    event.stopPropagation();
+	    $(this).toggleClass('show');
+		$(this).children('.dropdown-menu').toggleClass('show');
+	});
 	$('.navbar-toggler').click(function(){
 	$(this).next().toggleClass('show');
 
+	});
+
+	//button functionality
+	var element = '<div class="col-12 col-lg-9 col-md-12 col-s-12 bg-blue rounded mb-4" id="studentForm">\
+				<form class="was-validated pt-2">\
+				<label class="d-flex">\
+					<span>Student Form</span>\
+					<i id="cancel" class="ml-auto fas fa-times"></i>\
+				</label>\
+				  <div class="row form-group">\
+				    <div class="col">\
+				      <input type="<tex></tex>t" class="form-control" placeholder="First name" required>\
+				    </div>\
+				    <div class="col">\
+				      <input type="text" class="form-control" placeholder="Last name" required>\
+				    </div>\
+				  </div>\
+				  <div class="form-group">\
+				    <input type="text" class="form-control" placeholder="Phone Number" required>\
+				  </div>\
+				  <div class="form-group">\
+				    <input type="text" class="form-control" placeholder="Parent Number" required>\
+				  </div>\
+				  <div class="form-group raw">\
+				  	<select class="custom-select col" required>\
+					  <option class="respon-font">Study year</option>\
+					  <option value="1">One</option>\
+					  <option value="2">Two</option>\
+					  <option value="3">Three</option>\
+					</select>\
+				  </div>\
+				  <div class="form-group raw">\
+				  	<select class="custom-select col" required>\
+					  <option class="respon-font">select Group</option>\
+					  <option value="1">One</option>\
+					  <option value="2">Two</option>\
+					  <option value="3">Three</option>\
+					</select>\
+				  </div>\
+				  <div class="w-25 mx-auto form-group">\
+					<input type="submit" id="add-btn" class="btn" name="submit" value="Add">\
+				</div>\
+				</form>\
+			</div>';
+	$('.option-btn').click(function(){
+		if(!$("#studentForm").length){
+			$('#list-btn').after(element);
+		}
+	});
+	$('.container').on("click",'#cancel',function(){
+		$(this).parents('#studentForm').remove();
 	});
 });
